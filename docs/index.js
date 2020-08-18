2b76f508bd6d5728b813c7b2b7f95822eb4ba5e1
Dolf Andringa <dolfandringa@gmail.com>
2020-08-18 22:50:56 +0800

    change camera position

--- docs/index.js
+++ docs/index.js
@@ -15,7 +15,7 @@ function init() {
 
   camera = new THREE.PerspectiveCamera( 90, width / height, 1, 3000 );
   camera.up.set(0,-1,0);
-  camera.position.set(645, -2600, -3600);
+  camera.position.set(645, -1500, -3600);
   camera.lookAt(645, 0, -3600);
 
   scene = new THREE.Scene();
