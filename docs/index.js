7c4e0275ea1e4ddcfe193381471e3b8216bcc0a7
Dolf Andringa <dolfandringa@gmail.com>
2020-08-18 18:51:12 +0800

    show camera position

--- docs/index.js
+++ docs/index.js
@@ -14,8 +14,8 @@ init();
 function init() {
 
   camera = new THREE.PerspectiveCamera( 70, width / height, 1, 3000 );
-  camera.position.set(0, 500, -5000);
-  camera.lookAt(0, 0, -1000);
+  camera.position.set(0, 500, -4000);
+  camera.lookAt(0, 0, -2000);
 
   scene = new THREE.Scene();
 
