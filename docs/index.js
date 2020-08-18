9d71a90b9d99a43a430221c3ad06da1153090c33
Dolf Andringa <dolfandringa@gmail.com>
2020-08-18 16:24:53 +0800

    Added ply loader

--- docs/index.js
+++ docs/index.js
@@ -1,39 +1,48 @@
-import * as THREE from 'https://unpkg.com/three/build/three.module.js'
+import * as THREE from 'https://unpkg.com/three/build/three.module.js';
+import { PLYLoader } from 'https://unpkg.com/three/examples/jsm/loaders/PLYLoader.js';
 
 var camera, scene, renderer;
 var geometry, material, mesh;
 
 init();
-animate();
+//animate();
 
 function init() {
-    let width = 0.9 * window.innerWidth;
-    let height = 0.9 * window.innerHeight;
+  let width = 0.9 * window.innerWidth;
+  let height = 0.9 * window.innerHeight;
 
-    let camera = new THREE.PerspectiveCamera( 70, width / height, 0.01, 10 );
-    camera.position.z = 1;
+  camera = new THREE.PerspectiveCamera( 70, width / height, 0.01, 10 );
+  camera.position.z = 1;
 
-    let scene = new THREE.Scene();
+  scene = new THREE.Scene();
 
-    geometry = new THREE.BoxGeometry( 0.2, 0.2, 0.2 );
-    material = new THREE.MeshNormalMaterial();
+  let loader = new PLYLoader();
 
-    mesh = new THREE.Mesh( geometry, material );
-    scene.add( mesh );
+  /*geometry = new THREE.BoxGeometry( 0.2, 0.2, 0.2 );
+  material = new THREE.MeshNormalMaterial();
 
-    renderer = new THREE.WebGLRenderer( { antialias: true } );
-    renderer.setSize( width, height );
-    document.body.appendChild( renderer.domElement );
+  mesh = new THREE.Mesh( geometry, material );
+  scene.add( mesh );*/
+  console.log("Starting loading....");
+  loader.load('barnards_clusters.ply', function(geometry) {
+    console.log("Loaded ply file");
+    scene.add(new THREE.Mesh(geometry));
+    console.log("Added mesh");
+  });
+
+  renderer = new THREE.WebGLRenderer( { antialias: true } );
+  renderer.setSize( width, height );
+  document.body.appendChild( renderer.domElement );
 
 }
 
 function animate() {
 
-    requestAnimationFrame( animate );
+  requestAnimationFrame( animate );
 
-    mesh.rotation.x += 0.01;
-    mesh.rotation.y += 0.02;
+  mesh.rotation.x += 0.01;
+  mesh.rotation.y += 0.02;
 
-    renderer.render( scene, camera );
+  renderer.render( scene, camera );
 
 }
