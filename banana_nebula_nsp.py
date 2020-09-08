cf15cf83e0a25a4c6bf523454080ba6cce5ddb16
Dolf Andringa <dolfandringa@gmail.com>
2020-09-08 23:40:37 +0800

    Fixed pep8 stuff, updated the data, and autogenerate a csv version

--- banana_nebula_nsp.py
+++ banana_nebula_nsp.py
@@ -1,19 +1,19 @@
-import pickle
-import os.path
-from googleapiclient.discovery import build
-from google_auth_oauthlib.flow import InstalledAppFlow
-from google.auth.transport.requests import Request
-import pandas as pd
-import requests
-import matplotlib.pyplot as plt
-from functools import reduce
-import re
-import numpy as np
 import json
 import logging
+import os.path
+import pickle
+import re
+from functools import reduce
+
+import matplotlib.pyplot as plt
+import numpy as np
 import open3d as o3d
+import pandas as pd
+import requests
 from fuzzywuzzy import process
-import json
+from google.auth.transport.requests import Request
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
 
 logging.basicConfig(format='%(asctime)s-%(name)s: %(message)s',
                     level=logging.ERROR, datefmt='%Y-%m-%d %H:%M:%S')
@@ -42,16 +42,19 @@ species = {
 number_re = re.compile('^([0-9\.-]*).*')
 
 
-def get_matches(value, l):
-    matches = process.extract(value, l)
+def get_matches(value, txt):
+    matches = process.extract(value, txt)
+
     if matches[0][1] == 100:
         return [matches[0][0]]
+
     return [m[0] for m in matches if m[1] > 60]
 
 
 def isfloat(value):
     try:
         float(value)
+
         return True
     except ValueError:
         return False
@@ -60,11 +63,12 @@ def isfloat(value):
 def columnToSpecies(name):
     group, species = name.split('_', 1)
     species = " " .join([v.capitalize() for v in species.split('_')])
+
     return group, species
 
 
-def speciesToColumn(group, species):
-    return f"{group}_{species.lower().replace(', ','_')}"
+def speciesToColumn(group, sp):
+    return f"{group}_{sp.lower().replace(', ','_')}"
 
 
 species_column_names = [speciesToColumn(
@@ -75,6 +79,7 @@ def process_line(result, line):
     (systems, sites, rownum) = result
     rownum += 1
     log.debug("Line: %s", line)
+
     if line == [] or 'systems here' in line[0].lower():
         return (systems, sites, rownum)
 
@@ -89,7 +94,8 @@ def process_line(result, line):
 
     system = systems.setdefault(
         system_name, {'x': -1, 'y': -1, 'z': -1, 'num_sites': 0})
-    if len(sites):
+
+    if len(sites) > 0:
         prev_system_name = sites[-1][2]
         prev_system = systems[prev_system_name]
     else:
@@ -105,16 +111,19 @@ def process_line(result, line):
             pass
     # Pad with None values until we have the full 14 columns
     line = line + (14-len(line)) * [None]
-    if not any([l is None for l in line[11:14]]):
+
+    if not any([i is None for i in line[11:14]]):
         # We have 3 coordinate fields
         x = number_re.sub(r'\1', line[11].strip())
         y = number_re.sub(r'\1', line[12].strip())
         z = number_re.sub(r'\1', line[13].strip())
+
         if isfloat(x) and isfloat(y) and isfloat(z):
             # remove any additional characters after the coordinates
             system['x'] = float(x)
             system['y'] = float(y)
             system['z'] = float(z)
+
     if (system['x'], system['y'], system['z']) == (-1, -1, -1):
         if system_name == prev_system_name:
             system['x'] = prev_system['x']
@@ -128,23 +137,30 @@ def process_line(result, line):
     line[12] = system['y']
     line[13] = system['z']
 
-    # Replace empty strings with None and add the original row number from the google sheet
-    line = [rownum] + list(map(lambda x: None if isinstance(x,
-                                                            str) and x.strip() == '' else x, line))
+    # Replace empty strings with None and add the original row number from the
+    # google sheet
+    line = [rownum] + list(
+        map(lambda x: None if isinstance(x, str)
+            and x.strip() == '' else x, line))
 
     sites.append(line)
+
     return (systems, sites, rownum)
 
 
 def lookup_coords(system_name):
-    url = f"https://www.edsm.net/api-v1/system?showCoordinates=1&systemName={system_name}"
-    r = requests.get(url)
-    if r.status_code != 200:
+    url = "https://www.edsm.net/api-v1/system?"\
+          f"showCoordinates=1&systemName={system_name}"
+    res = requests.get(url)
+
+    if res.status_code != 200:
         return (-1, -1, -1)
-    json = r.json()
+    json = res.json()
+
     if json == [] or 'coords' not in json.keys():
         return (-1, -1, -1)
     coords = json['coords']
+
     return (coords['x'], coords['y'], coords['z'])
 
 
@@ -154,6 +170,7 @@ def get_creds():
     # created automatically when the authorization flow completes for the first
     # time.
     scopes = ['https://www.googleapis.com/auth/spreadsheets.readonly']
+
     if os.path.exists('token.pickle'):
         with open('token.pickle', 'rb') as token:
             creds = pickle.load(token)
@@ -169,26 +186,29 @@ def get_creds():
         # Save the credentials for the next run
         with open('token.pickle', 'wb') as token:
             pickle.dump(creds, token)
+
     return creds
 
 
 def read_gsheet():
     # The ID and range of a sample spreadsheet.
-    SAMPLE_SPREADSHEET_ID = '1k5gf13mvScxpxxUYWxFWG08mBYB6AXiRLUrtFJj_2Y0'
-    START_ROW = 3
-    END_ROW = 5000
-    SAMPLE_RANGE_NAME = f"NSP SYSTEM DATA!A{START_ROW}:O{END_ROW}"
+    sample_spreadsheet_id = '1k5gf13mvScxpxxUYWxFWG08mBYB6AXiRLUrtFJj_2Y0'
+    start_row = 3
+    end_row = 5000
+    sample_range_name = f"NSP SYSTEM DATA!A{start_row}:O{end_row}"
     creds = get_creds()
     service = build('sheets', 'v4', credentials=creds)
     sheet = service.spreadsheets()
-    result = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
-                                range=SAMPLE_RANGE_NAME).execute()
+    result = sheet.values().get(spreadsheetId=sample_spreadsheet_id,
+                                range=sample_range_name).execute()
     sites = []
     systems = {}
     reduce(process_line, result.get('values', []),
-           (systems, sites, START_ROW-1))
-    columns = ['gsheet_rownum', 'added_by', 'system_name', 'mollusc', 'lagrange_cloud', 'ice_crystal', 'metallic_crystal',
-               'silicate_crystal', 'solid_mineral', 'empty1', 'distance_to_nearest_star', 'empty2', 'x', 'y', 'x']
+           (systems, sites, start_row-1))
+    columns = ['gsheet_rownum', 'added_by', 'system_name', 'mollusc',
+               'lagrange_cloud', 'ice_crystal', 'metallic_crystal',
+               'silicate_crystal', 'solid_mineral', 'empty1',
+               'distance_to_nearest_star', 'empty2', 'x', 'y', 'x']
     sites = pd.DataFrame(sites, columns=columns)
     systems = pd.DataFrame.from_dict(systems, orient='index')
 
@@ -198,18 +218,24 @@ def read_gsheet():
 def calculate_columns(systems, sites):
     systems = systems.copy()
     sites = sites.copy()
-    for col in species.keys():
+
+    for col in species:
         for sp in species[col]:
             sites[f"{col}_{sp.lower().replace(', ','_')}"] = sites.apply(
-                lambda row: row[col] is not None and sp in get_matches(row[col], species[col]), axis=1)
+                lambda row: row[col] is not None and
+                sp in get_matches(row[col], species[col]), axis=1)
     agg_columns = dict([(v, np.max) for v in species_column_names])
     agg_columns['added_by'] = lambda x: x.iloc[0]
     systems = systems.join(sites[['system_name', 'added_by'] +
-                                 species_column_names].groupby(by=['system_name']).agg(agg_columns))
-    for group in species.keys():
+                                 species_column_names]
+                           .groupby(by=['system_name']).agg(agg_columns))
+
+    for group in species:
         columns = [k for k in species_column_names if k.startswith(group)]
         systems[group] = systems.apply(lambda row: ", ".join(
-            [columnToSpecies(col)[1] for col in columns if row[col]]) or None, axis=1)
+            [columnToSpecies(col)[1] for col in columns if row[col]]) or None,
+            axis=1)
+
     return systems, sites
 
 
@@ -218,9 +244,16 @@ def save_data(systems, sites):
     sites.to_hdf('banana_nebula_nsp.h5', 'sites', format='table')
 
 
+def save_csv(systems):
+    systems.to_csv('banana_nebula_nsp.csv')
+
+
 def save_layers(layers, tables):
+    """Save layer PCB and json files"""
+
     for k, v in layers.items():
         o3d.io.write_point_cloud(f'docs/{k}.pcd', v)
+
     for k, v in tables.items():
         with open(f'docs/{k}.json', 'w') as f:
             f.write(json.dumps(v))
@@ -230,6 +263,7 @@ def load_data():
     store = pd.HDFStore('banana_nebula_nsp.h5')
     systems = store.get('/systems')
     sites = store.get('/sites')
+
     return (systems, sites)
 
 
@@ -237,8 +271,9 @@ def generate_layers(systems):
     pcd = o3d.geometry.PointCloud()
     color = (0.0, 0.0, 0.0)
 
-    json_columns = ['system_name', 'added_by', 'num_sites', 'mollusc', 'lagrange_cloud',
-                    'ice_crystal', 'metallic_crystal', 'silicate_crystal', 'solid_mineral', 'x', 'y', 'z']
+    json_columns = ['system_name', 'added_by', 'num_sites', 'mollusc',
+                    'lagrange_cloud', 'ice_crystal', 'metallic_crystal',
+                    'silicate_crystal', 'solid_mineral', 'x', 'y', 'z']
 
     systems['system_name'] = systems.index
     layer_systems = systems.replace(
@@ -254,6 +289,7 @@ def generate_layers(systems):
     num_groups = len(species.keys())
     layers = {'all_systems': pcd}
     tables = {'all_systems': d}
+
     for i, group in enumerate(species.keys()):
         color = plt.cm.viridis(i/num_groups)
         pcd = o3d.geometry.PointCloud()
@@ -267,13 +303,16 @@ def generate_layers(systems):
 
         d = layer_systems[json_columns].to_dict('records')
         tables[group] = d
+
     return layers, tables
 
 
 def main():
+    """Main function that imports all NSP data."""
     systems, sites = read_gsheet()
     systems, sites = calculate_columns(systems, sites)
     layers, tables = generate_layers(systems[systems['x'] != -1])
+    save_csv(systems)
     save_data(systems, sites)
     save_layers(layers, tables)
 
